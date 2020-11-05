d6dc85986d4f45a78946c3a16d6db40638f6a708
htrefil <8711792+htrefil@users.noreply.github.com>
2020-11-05 18:11:49 +0100

    Update to tokio 0.3

--- src/stream.rs
+++ src/stream.rs
@@ -1,25 +1,17 @@
 use std::{
     io,
-    ops::Deref,
+    os::unix::io::{AsRawFd, RawFd},
     pin::Pin,
     sync::Arc,
-    task::{Poll, Context},
+    task::{Context, Poll},
 };
 
-use mio::{
-    event::Evented,
-    unix::EventedFd,
-};
-use tokio::io::{AsyncRead, PollEvented};
-use futures_core::{Stream, ready};
+use futures_core::{ready, Stream};
+use tokio::io::unix::AsyncFd;
 
-use crate::events::{
-    Event,
-    EventOwned,
-};
+use crate::events::{Event, EventOwned};
 use crate::fd_guard::FdGuard;
 use crate::util::read_into_buffer;
-
 
 /// Stream of inotify events
 ///
@@ -27,7 +19,7 @@ use crate::util::read_into_buffer;
 ///
 /// [`Inotify::event_stream`]: struct.Inotify.html#method.event_stream
 pub struct EventStream<T> {
-    fd: PollEvented<EventedFdGuard>,
+    fd: AsyncFd<ArcFdGuard>,
     buffer: T,
     buffer_pos: usize,
     unused_bytes: usize,
@@ -40,7 +32,7 @@ where
     /// Returns a new `EventStream` associated with the default reactor.
     pub(crate) fn new(fd: Arc<FdGuard>, buffer: T) -> io::Result<Self> {
         Ok(EventStream {
-            fd: PollEvented::new(EventedFdGuard(fd))?,
+            fd: AsyncFd::new(ArcFdGuard(fd))?,
             buffer: buffer,
             buffer_pos: 0,
             unused_bytes: 0,
@@ -54,15 +46,14 @@ where
 {
     type Item = io::Result<EventOwned>;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>>
-    {
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         // Safety: safe because we never move out of `self_`.
         let self_ = unsafe { self.get_unchecked_mut() };
 
         if self_.unused_bytes == 0 {
             // Nothing usable in buffer. Need to reset and fill buffer.
-            self_.buffer_pos   = 0;
-            self_.unused_bytes = ready!(Pin::new(&mut self_.fd).poll_read(cx, self_.buffer.as_mut()))?;
+            self_.buffer_pos = 0;
+            self_.unused_bytes = ready!(read(&self_.fd, self_.buffer.as_mut(), cx))?;
         }
 
         if self_.unused_bytes == 0 {
@@ -75,62 +66,39 @@ where
         // there, and we only take complete events out. That means we have at
         // least one event in there and can call `from_buffer` to take it out.
         let (bytes_consumed, event) = Event::from_buffer(
-            Arc::downgrade(self_.fd.get_ref()),
+            Arc::downgrade(&self_.fd.get_ref().0),
             &self_.buffer.as_ref()[self_.buffer_pos..],
         );
-        self_.buffer_pos   += bytes_consumed;
+        self_.buffer_pos += bytes_consumed;
         self_.unused_bytes -= bytes_consumed;
 
         Poll::Ready(Some(Ok(event.into_owned())))
     }
 }
 
-#[derive(Clone, Debug, PartialEq)]
-struct EventedFdGuard(Arc<FdGuard>);
+// Newtype wrapper because AsRawFd isn't implemented for Arc<T> where T: AsRawFd.
+struct ArcFdGuard(Arc<FdGuard>);
 
-impl Evented for EventedFdGuard {
-    #[inline]
-    fn register(&self,
-                poll: &mio::Poll,
-                token: mio::Token,
-                interest: mio::Ready,
-                opts: mio::PollOpt)
-                -> io::Result<()>
-    {
-        EventedFd(&(self.fd)).register(poll, token, interest, opts)
-    }
-
-    #[inline]
-    fn reregister(&self,
-                  poll: &mio::Poll,
-                  token: mio::Token,
-                  interest: mio::Ready,
-                  opts: mio::PollOpt)
-                  -> io::Result<()>
-    {
-        EventedFd(&(self.fd)).reregister(poll, token, interest, opts)
-    }
-
-    #[inline]
-    fn deregister(&self, poll: &mio::Poll) -> io::Result<()> {
-        EventedFd(&self.fd).deregister(poll)
+impl AsRawFd for ArcFdGuard {
+    fn as_raw_fd(&self) -> RawFd {
+        self.0.as_raw_fd()
     }
 }
 
-impl io::Read for EventedFdGuard {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        match read_into_buffer(self.fd, buf) {
-            i if i >= 0 => Ok(i as usize),
-            _ => Err(io::Error::last_os_error()),
+fn read(fd: &AsyncFd<ArcFdGuard>, buffer: &mut [u8], cx: &mut Context) -> Poll<io::Result<usize>> {
+    let mut guard = ready!(fd.poll_read_ready(cx))?;
+    let result = guard.with_io(|| {
+        let read = read_into_buffer(fd.as_raw_fd(), buffer);
+        if read == -1 {
+            return Err(io::Error::last_os_error());
         }
-    }
-}
 
-impl Deref for EventedFdGuard {
-    type Target = Arc<FdGuard>;
-    #[inline]
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
+        Ok(read as usize)
+    });
 
+    match result {
+        Ok(read) => Poll::Ready(Ok(read)),
+        Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => Poll::Pending,
+        Err(err) => Poll::Ready(Err(err)),
+    }
 }
